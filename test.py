df0b44272c2abab02e6067ed05e21b82f08e22f4
Nathan Singer <nathan.r.singer@gmx.com>
2025-11-25 16:43:10 -0500

    formatting changes

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import json
 from patch_note import patch_note
+import difflib
+import sys
+
 
 with open("tests.json") as file:
     tests = json.load(file)
@@ -11,9 +14,12 @@ model = patch_note()
 for _, value in tests.items():
 
     out = model.get_response(value["input"])
+    print(json.dumps(json.loads(out)))
 
-    print(out)
-    
+    print(value["output"])
+
+    #sys.stdout.writelines(difflib.unified_diff(out, value["output"]))
+
     #print(value["output"])
     #print(json.dumps(json.loads(value["output"]), indent=4))
 
